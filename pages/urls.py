d6a65fdbcc644877e30f1c9be15fb280430db2cb
Thomas <tahjones75@gmail.com>
2020-07-07 20:36:41 +0000

    change url path syntax for function based views

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import HomePageView, HomeopathyPageView, AboutPageView, ContactPageView
+# from .views import HomePageView, HomeopathyPageView, AboutPageView, ContactPageView
+from pages import views
 
+
+# urlpatterns = [
+#     path('', HomePageView.as_view(), name='index'),
+#     path('homeopathy/', HomeopathyPageView.as_view(), name='homeopathy'),
+#     path('about/', AboutPageView.as_view(), name='about'),
+#     path('contact/', ContactPageView.as_view(), name='contact'),
+# ]
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='index'),
-    path('homeopathy/', HomeopathyPageView.as_view(), name='homeopathy'),
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('contact/', ContactPageView.as_view(), name='contact'),
+    path('', views.home, name='index'),
+    path('homeopathy/', views.homeopathy, name='homeopathy'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]
