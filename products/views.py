be7ed489a2c8f05197ffcba8ef46c5c2f9f02e26
Thomas <tahjones75@gmail.com>
2020-07-18 07:53:01 +0000

    added product_detail view

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
+    """ A view to show all remedies and remedy kits, including sorting and search queries """
 
     products = Product.objects.all()
 
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual remedies and remedy kits """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product_detail.html', context)
