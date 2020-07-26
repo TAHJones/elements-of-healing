139985f92375b016aabd4fbe019d57f3250f4c05
Thomas <tahjones75@gmail.com>
2020-07-26 09:13:05 +0000

    fixed url path name for basket

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from basket import views
 
 urlpatterns = [
-    path('', views.view_basket, name='view_basket')
+    path('basket/', views.view_basket, name='view_basket')
 ]
