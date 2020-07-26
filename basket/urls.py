f0e57be4b512fe53ca86713dbdc1a63a749e6321
Thomas <tahjones75@gmail.com>
2020-07-26 17:12:05 +0000

    added url for basket app

--- basket/urls.py
+++ basket/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from basket import views
 
 urlpatterns = [
-    path('basket/', views.view_basket, name='view_basket')
+    path('basket/', views.view_basket, name='view_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
