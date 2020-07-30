2bc884a008cda73c45e3819f2ec534e37647873e
Thomas <tahjones75@gmail.com>
2020-07-30 18:51:40 +0000

    changed session var name from bag to basket

--- basket/views.py
+++ basket/views.py
@@ -14,13 +14,13 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    bag = request.session.get('bag', {})
+    basket = request.session.get('basket', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
     else:
-        bag[item_id] = quantity
+        basket[item_id] = quantity
 
-    request.session['bag'] = bag
-    print(request.session['bag'])
+    request.session['basket'] = basket
+    # request.session['basket'] = {}
     return redirect(redirect_url)
