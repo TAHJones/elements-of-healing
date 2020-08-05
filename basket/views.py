69c14e006c37d4818cdeaeee1a71d6d559b33dee
Thomas <tahjones75@gmail.com>
2020-08-05 15:33:49 +0000

    added logic to handle purchase mutiple remedy potencies

--- basket/views.py
+++ basket/views.py
@@ -14,13 +14,25 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    potency = None
+    if 'potency' in request.POST:
+        potency = request.POST['potency']
+
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if potency:
+        if item_id in list(basket.keys()):
+            if potency in basket[item_id]['items_by_potency'].keys():
+                basket[item_id]['items_by_potency'][potency] += quantity
+            else:
+                basket[item_id]['items_by_potency'][potency] = quantity
+        else:
+            basket[item_id] = {'items_by_potency': {potency: quantity}}
     else:
-        basket[item_id] = quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
-    # request.session['basket'] = {}
     return redirect(redirect_url)
