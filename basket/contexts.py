2fa85bcbb61c498fd9ec5c4566218ce3b5f9b3b6
Thomas <tahjones75@gmail.com>
2020-08-12 15:45:55 +0000

    removed old commented out code

--- basket/contexts.py
+++ basket/contexts.py
@@ -10,17 +10,6 @@ def basket_contents(request):
     total = 0
     product_count = 0
     basket = request.session.get('basket', {})
-    # basket = {}
-
-    # for item_id, quantity in basket.items():
-    #     product = get_object_or_404(Product, pk=item_id)
-    #     total += quantity * product.price
-    #     product_count += quantity
-    #     basket_items.append({
-    #         'item_id': item_id,
-    #         'quantity': quantity,
-    #         'product': product,
-    #     })
 
     for item_id, item_data in basket.items():
         if isinstance(item_data, int):
