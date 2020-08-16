3dba14f3c7f9156b22f66b3e917c64dc61e9f2d2
Thomas <tahjones75@gmail.com>
2020-08-16 07:02:50 +0000

    added url path for checkou_success view

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,4 +3,5 @@ from checkout import views
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
 ]
