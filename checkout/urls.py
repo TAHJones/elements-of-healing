af362703437835acbd7073ae69d3ebfcb14f3671
Thomas <tahjones75@gmail.com>
2020-09-05 07:00:43 +0000

    added url path for webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from checkout import views
+from checkout import webhooks
 
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhooks.webhook, name='webhook'),
 ]
