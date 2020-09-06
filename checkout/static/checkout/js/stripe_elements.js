61e7c1e2e4a8cab16244546ebffc19a0f390a3ec
Thomas <tahjones75@gmail.com>
2020-09-06 12:28:17 +0000

    removed bases styles for stripe elements

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -10,15 +10,6 @@ var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
-    // base: {
-    //     color: '#000',
-    //     fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    //     fontSmoothing: 'antialiased',
-    //     fontSize: '16px',
-    //     '::placeholder': {
-    //         color: '#aab7c4'
-    //     }
-    // },
     invalid: {
         color: '#dc3545',
         iconColor: '#dc3545'
