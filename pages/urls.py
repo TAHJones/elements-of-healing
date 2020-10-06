4cdaacb6e46180cbb3395a32657b34467ae6ef71
Thomas <tahjones75@gmail.com>
2020-10-06 20:37:52 +0000

    changed root url name from index to home & removed commented out code

--- pages/urls.py
+++ pages/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
-# from .views import HomePageView, HomeopathyPageView, AboutPageView, ContactPageView
 from pages import views
 
 
-# urlpatterns = [
-#     path('', HomePageView.as_view(), name='index'),
-#     path('homeopathy/', HomeopathyPageView.as_view(), name='homeopathy'),
-#     path('about/', AboutPageView.as_view(), name='about'),
-#     path('contact/', ContactPageView.as_view(), name='contact'),
-# ]
-
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.home, name='home'),
     path('homeopathy/', views.homeopathy, name='homeopathy'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
 ]
