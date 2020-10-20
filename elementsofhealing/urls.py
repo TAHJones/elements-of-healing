04547aae634921ab9f88225a2cc3983280f838ca
Thomas <tahjones75@gmail.com>
2020-10-20 20:23:54 +0000

    added path for contacts url

--- elementsofhealing/urls.py
+++ elementsofhealing/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    path('', include('contact.urls')),
     path('accounts/', include('allauth.urls')),
     path('', include('products.urls')),
     path('basket/', include('basket.urls')),
