c576f56c4d9101c3e13e14ea700e9ade2656074f
Thomas <tahjones75@gmail.com>
2020-10-22 20:17:14 +0000

    added url path for success view

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,5 @@ from contact import views
 
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
-    path('contact/success/<email_from>', views.successView, name='success'),
+    path('contact/success/<email_fromview>', views.successView, name='success'),
 ]
