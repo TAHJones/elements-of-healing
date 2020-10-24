a8a54119c745222803bce1de75ce582cbb99d331
Thomas <tahjones75@gmail.com>
2020-10-24 14:27:28 +0000

    deleted url path for success view

--- contact/urls.py
+++ contact/urls.py
@@ -3,5 +3,4 @@ from contact import views
 
 urlpatterns = [
     path('contact/', views.contact, name='contact'),
-    path('contact/success/<email_fromview>', views.successView, name='success'),
 ]
