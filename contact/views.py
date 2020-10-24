238ccb4d132be9140d629eae9894f743f984dabe
Thomas <tahjones75@gmail.com>
2020-10-24 14:29:23 +0000

    added code to send confirmation email to contact view & deleted succesView view

--- contact/views.py
+++ contact/views.py
@@ -1,22 +1,10 @@
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail, BadHeaderError
+from django.shortcuts import render, redirect, reverse
+from django.core.mail import send_mail
 from django.http import HttpResponse
-# from django.http import HttpResponse, HttpResponseRedirect
+from django.template.loader import render_to_string
 from django.conf import settings
 from django.contrib import messages
 from .forms import ContactForm
-
-
-# def contact(request):
-#     if request.method == 'POST':
-#         form = ContactForm(request.POST)
-#         if form.is_valid():
-#             # send email code goes here
-#             return HttpResponse('Thanks for contacting us!')
-#     else:
-#         form = ContactForm()
-
-#     return render(request, 'contact/contact.html', {'form': form})
 
 
 def contact(request):
@@ -26,19 +14,30 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
-            email_from = form.cleaned_data['email']
+            cust_email = form.cleaned_data['email']
             message = form.cleaned_data['message']
-            email_to = settings.DEFAULT_FROM_EMAIL
+            host_email = settings.DEFAULT_FROM_EMAIL
+            # email_to = settings.EMAIL_HOST_USER
+            email = {
+                'name': name,
+                'cust_email': cust_email,
+                'message': message,
+                'host_email': host_email,
+            }
+            subject = render_to_string(
+                'confirmation_emails/confirmation_email_subject.txt',
+                {'email': email})
+            body = render_to_string(
+                'confirmation_emails/confirmation_email_body.txt',
+                {'email': email})
             try:
-                send_mail(name, message, email_from, [email_to])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            return redirect('success', email_from)
+                # forward message from customer to host email address
+                send_mail(name, message, cust_email, [host_email])
+                messages.success(request, f'Your message has been received! A confirmation email will be sent to {cust_email}.')
+                # send confirmation message from host to customer email address
+                send_mail(subject, body, host_email, [cust_email])
+            except Exception as e:
+                messages.error(request, 'Sorry, there was a problem sending your message. Please try again.')
+                return HttpResponse(content=e, status=400)
+            return redirect(reverse('contact'))
     return render(request, 'contact/contact.html', {'form': form})
-
-
-def successView(request, email_from):
-    messages.success(request, f'Your message has been recieved! A confirmation email will be sent to {email_from}.')
-    # return HttpResponse('Success! Thank you for your message.')
-    # return render(request, 'contact/contact.html', {'form': form})
-    return render(request, 'contact/success.html')
