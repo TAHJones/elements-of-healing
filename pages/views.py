3195b4b1ad1d83b16a5d1a48fec9e86fcca6aa2d
Thomas <tahjones75@gmail.com>
2020-10-24 17:00:13 +0000

    removed commited out code from pages views

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import render, redirect
-# from django.shortcuts import render
-# from django.views.generic import TemplateView
-
-# Create your views here.
-
-# class HomePageView(TemplateView):
-#     template_name = 'index.html'
+from django.shortcuts import render
 
 
 def home(request):
@@ -15,18 +8,11 @@ def home(request):
     return render(request, "index.html", context)
 
 
-# class HomeopathyPageView(TemplateView):
-#     template_name = 'homeopathy.html'
-
-
 def homeopathy(request):
     context = {
         'banner': True
     }
     return render(request, "homeopathy.html", context)
-
-# class AboutPageView(TemplateView):
-#     template_name = 'about.html'
 
 
 def about(request):
@@ -35,9 +21,6 @@ def about(request):
     }
     return render(request, "about.html", context)
 
-
-# class ContactPageView(TemplateView):
-#     template_name = 'contact.html'
 
 
 def contact(request):
