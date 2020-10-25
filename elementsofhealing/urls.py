0980fd31cc2c733ddd6557c021f1b302022a664f
Thomas <tahjones75@gmail.com>
2020-10-25 16:27:26 +0000

    update url path for products app

--- elementsofhealing/urls.py
+++ elementsofhealing/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('', include('contact.urls')),
     path('accounts/', include('allauth.urls')),
-    path('', include('products.urls')),
+    path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from products import views
 
 urlpatterns = [
-    path('products/', views.all_products, name='products'),
+    path('', views.all_products, name='products'),
     path('<int:product_id>/', views.product_detail, name='product_detail'),
-    path('products/add/', views.add_product, name='add_product'),
-    path('products/update/<int:product_id>/', views.update_product, name='update_product'),
-    path('products/delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('add/', views.add_product, name='add_product'),
+    path('update/<int:product_id>/', views.update_product, name='update_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
