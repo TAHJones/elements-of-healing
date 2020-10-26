442f7bfe0364802c92d812366d3866d1d605829d
Thomas <tahjones75@gmail.com>
2020-10-26 20:06:09 +0000

    removed form field models

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
-
-class Contact(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=50, null=False, blank=False)
-    message = models.TextField(max_length=300, null=False, blank=False)
+# Create your models here.
