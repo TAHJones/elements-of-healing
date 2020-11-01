88fa9c73f338d630c177282265a815d1702be163
Thomas <tahjones75@gmail.com>
2020-11-01 15:55:26 +0000

    changed date & time field to character field

--- appointments/models.py
+++ appointments/models.py
@@ -5,8 +5,8 @@ class Appointments(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     message = models.TextField(max_length=300, null=True, blank=True)
-    date = models.DateField(null=False, blank=False)
-    time = models.TimeField(null=False, blank=False)
+    date = models.CharField(max_length=10, null=False, blank=False)
+    time = models.CharField(max_length=10, null=True, blank=True)
 
     def __str__(self):
         return self.name
