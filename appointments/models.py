73b75ef42abb3094b72901da9a2bb34104f39894
Thomas <tahjones75@gmail.com>
2020-11-05 20:16:10 +0000

    changed model name to BookAppointment

--- appointments/models.py
+++ appointments/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Appointments(models.Model):
+class BookAppointment(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     message = models.TextField(max_length=300, null=True, blank=True)
