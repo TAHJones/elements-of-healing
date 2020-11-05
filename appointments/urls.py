c51981a642dda68b22c020e3faf9e6a30f51780f
Thomas <tahjones75@gmail.com>
2020-11-05 20:17:58 +0000

    update appointments url pathnames

--- appointments/urls.py
+++ appointments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from appointments import views
 
 urlpatterns = [
-    path('', views.appointments, name='appointments'),
+    path('<int:product_id>/', views.appointments, name='appointments'),
+    path('purchase_appointment/', views.purchaseAppointment, name='purchase_appointment'),
 ]
