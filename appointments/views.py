e3276030c36088c93feac2b6ec52a5660ea6f1f4
Thomas <tahjones75@gmail.com>
2020-11-07 08:30:19 +0000

    removed code to handle emails from appointments view

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.core.mail import send_mail
-from django.http import HttpResponse
-from django.template.loader import render_to_string
 from django.conf import settings
 from django.contrib import messages
 from .forms import AppointmentForm
@@ -30,21 +27,6 @@ def appointments(request, product_id):
                 'time': time,
                 'host_email': host_email,
             }
-            subject = render_to_string(
-                'appointments/confirmation_emails/confirmation_email_subject.txt',
-                {'email': appointment_details})
-            body = render_to_string(
-                'appointments/confirmation_emails/confirmation_email_body.txt',
-                {'email': appointment_details})
-            try:
-                # forward message from customer to host email address
-                send_mail(name, message, cust_email, [host_email])
-                messages.success(request, f'Your appointment request has been received! A confirmation email will be sent to {cust_email}.')
-                # send confirmation message from host to customer email address
-                send_mail(subject, body, host_email, [cust_email])
-            except Exception as e:
-                messages.error(request, 'Sorry, there was a problem sending your appointment request. Please try again.')
-                return HttpResponse(content=e, status=400)
             request.session['appointment_id'] = product_id
             request.session['appointment_details'] = appointment_details
             return redirect(reverse('purchase_appointment'))
