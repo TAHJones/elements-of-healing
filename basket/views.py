2423de16fcfb453e6597ac6169b2d6770235c92d
Thomas <tahjones75@gmail.com>
2020-11-07 08:32:42 +0000

    added code to add appointments to basket & send email confirmations

--- basket/views.py
+++ basket/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from products.models import Product
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 
 
 def view_basket(request):
@@ -11,17 +13,44 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """Add a specific quantity of a selected product to the shopping basket"""
-
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    appointment_details = request.session['appointment_details']
     potency = None
     if 'potency' in request.POST:
         potency = request.POST['potency']
 
     basket = request.session.get('basket', {})
 
-    if potency:
+    if product.category.friendly_name == "Appointments":
+        if item_id in list(basket.keys()):
+            messages.error(request, 'Error, you already have an appointment booked')
+        else:
+            name = appointment_details['name']
+            cust_email = appointment_details['cust_email']
+            message = appointment_details['message']
+            # date = appointment_details['date']
+            # time = appointment_details['time']
+            host_email = appointment_details['host_email']
+
+            subject = render_to_string(
+                'appointments/confirmation_emails/confirmation_email_subject.txt',
+                {'email': appointment_details})
+            body = render_to_string(
+                'appointments/confirmation_emails/confirmation_email_body.txt',
+                {'email': appointment_details})
+            try:
+                # forward message from customer to host email address
+                basket[item_id] = quantity
+                send_mail(name, message, cust_email, [host_email])
+                messages.success(request, f'Your appointment request has been received. A confirmation email will be sent to {cust_email}.')
+                # send confirmation message from host to customer email address
+                send_mail(subject, body, host_email, [cust_email])
+            except Exception as e:
+                messages.error(request, 'Sorry, there was a problem sending your appointment request. Please try again.')
+                return HttpResponse(content=e, status=400)
+    elif potency:
         if item_id in list(basket.keys()):
             if potency in basket[item_id]['items_by_potency'].keys():
                 basket[item_id]['items_by_potency'][potency] += quantity
