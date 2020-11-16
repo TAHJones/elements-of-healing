1d1a7f60b49bcb0219c900599009b743ff8fa15e
Thomas <tahjones75@gmail.com>
2020-11-16 21:57:27 +0000

    added BookAppointment model to admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BookAppointment
 
-# Register your models here.
+
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'message',
+        'date',
+        'time',
+    )
+
+    ordering = ('date',)
+
+
+admin.site.register(BookAppointment, AppointmentAdmin)
