8b9e5bfbbfd6e2de4f0a2531c8573d470d4dc239
Thomas <tahjones75@gmail.com>
2020-11-16 21:59:13 +0000

    added url path for appointmentCalendar view

--- appointments/urls.py
+++ appointments/urls.py
@@ -4,4 +4,5 @@ from appointments import views
 urlpatterns = [
     path('<int:product_id>/', views.appointments, name='appointments'),
     path('purchase_appointment/', views.purchaseAppointment, name='purchase_appointment'),
+    path('appointment_calendar/', views.appointmentCalendar, name='appointment_calendar'),
 ]
