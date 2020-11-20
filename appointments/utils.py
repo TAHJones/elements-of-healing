d23f3a7634b79d28ca92a756d8009a0823aed131
Thomas <tahjones75@gmail.com>
2020-11-20 12:52:07 +0000

    removed import statement for BookAppointment

--- appointments/utils.py
+++ appointments/utils.py
@@ -1,7 +1,5 @@
-# from datetime import datetime, timedelta
 from calendar import HTMLCalendar
-# from .models import Event
-from .models import BookAppointment, AppointmentsCalendar
+from .models import AppointmentsCalendar
 
 
 class Calendar(HTMLCalendar):
