9c2bffce3060bd7b4efb44ca4cf4f798c93843be
Thomas <tahjones75@gmail.com>
2020-11-20 12:51:04 +0000

    deleted BookAppointments model

--- appointments/models.py
+++ appointments/models.py
@@ -3,22 +3,12 @@ from datetime import datetime
 from django.urls import reverse
 
 
-class BookAppointment(models.Model):
-    name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=50, null=False, blank=False)
-    message = models.TextField(max_length=300, null=True, blank=True)
-    date = models.CharField(max_length=10, null=False, blank=False)
-    time = models.CharField(max_length=10, null=False, blank=False)
-
-    def __str__(self):
-        return self.name
-
-
 class AppointmentsCalendar(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     message = models.TextField(max_length=300, null=True, blank=True)
     date = models.DateTimeField(default=datetime.today)
+    date_str = models.CharField(max_length=10, null=False, blank=False)
     time = models.CharField(max_length=10, null=False, blank=False)
 
     def __str__(self):
