39b0bc889429ae34729ef8587cf99d12c345aec2
Thomas <tahjones75@gmail.com>
2020-11-21 18:43:37 +0000

    make appointmentCalendar a class based view & add get_date, prev_month & next_month functions

--- appointments/views.py
+++ appointments/views.py
@@ -5,8 +5,11 @@ from .forms import AppointmentForm
 from appointments.models import AppointmentsCalendar
 from products.models import Product
 from .utils import Calendar
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.utils.safestring import mark_safe
+from django.views import generic
+from django.urls import reverse_lazy
+import calendar
 
 
 def appointments(request, product_id):
@@ -72,24 +75,45 @@ def purchaseAppointment(request):
     return render(request, 'appointments/purchase_appointment.html', context)
 
 
-def appointmentCalendar(request):
-    """  Displays a calendar of all appointments """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry you must have admin privileges to view appointment bookings')
-        return redirect(reverse('appointments'))
+class appointmentCalendar(generic.ListView):
+    model = AppointmentsCalendar
+    template_name = 'appointments/appointment_calendar.html'
+    success_url = reverse_lazy("appointment_calendar")
 
-    d = datetime.today()
-    cal = Calendar(d.year, d.month)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        d = get_date(self.request.GET.get('month', None))
+        cal = Calendar(d.year, d.month)
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+        return context
 
-    # Call the formatmonth method, which returns our calendar as a table
-    html_cal = cal.formatmonth(withyear=True)
-    calendar = mark_safe(html_cal)
 
-    context = {
-        'calendar': calendar,
-    }
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        print(year)
+        print(month)
 
-    return render(request, 'appointments/appointment_calendar.html', context)
+        return datetime(year, month, day=1)
+    return datetime.today()
+
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
 
 
 def appointmentDetails(request,  appointment_details_id):
