ed733532d38b9dc16e139fadf531d8b63a9cbe81
Thomas <tahjones75@gmail.com>
2020-11-22 18:33:24 +0000

    added url path for confirmAppointment view

--- appointments/urls.py
+++ appointments/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('purchase_appointment/', views.purchaseAppointment, name='purchase_appointment'),
     path('appointment_calendar/', views.appointmentCalendar.as_view(), name='appointment_calendar'),
     path('appointment_details/<int:appointment_details_id>/', views.appointmentDetails, name='appointment_details'),
+    path('confirm_appointment/<int:appointment_details_id>/', views.confirmAppointment, name='confirm_appointment'),
 ]
