d16858d162e13a31f39f2c3666ed2415b6173350
Thomas <tahjones75@gmail.com>
2020-11-24 20:59:03 +0000

    add confirmed boolean field to AppointmentsCalendar model

--- appointments/models.py
+++ appointments/models.py
@@ -10,6 +10,7 @@ class AppointmentsCalendar(models.Model):
     date = models.DateTimeField(default=datetime.today)
     date_str = models.CharField(max_length=10, null=False, blank=False)
     time = models.CharField(max_length=10, null=False, blank=False)
+    confirmed = models.BooleanField(default=False, null=False, blank=False)
 
     def __str__(self):
         return self.name
