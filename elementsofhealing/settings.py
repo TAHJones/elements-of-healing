11a26dca8f3c4395208a383c671bf2d7902bc0ae
Thomas <tahjones75@gmail.com>
2020-11-29 20:04:28 +0000

    updated aws settings

--- elementsofhealing/settings.py
+++ elementsofhealing/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = True
 
 
 ALLOWED_HOSTS = ['elementsofhealing.herokuapp.com', 'localhost']
@@ -197,7 +198,9 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    # AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3-website.eu-west-2.amazonaws.com'
+    # http://elementsofhealing.s3-website.eu-west-2.amazonaws.com/
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
