829f39560271c10318317454056b890548ffea00
Thomas <tahjones75@gmail.com>
2020-11-29 12:34:21 +0000

    update settings for heroku deployment

--- elementsofhealing/settings.py
+++ elementsofhealing/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'tb(@v8v)8e5gqj%u%4f)k)ib7=4uk(-oe)&1=_5+$^994wo!1p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elementsofhealing.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -130,12 +130,17 @@ WSGI_APPLICATION = 'elementsofhealing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
