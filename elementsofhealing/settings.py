939ee7925b68b7171958c71189b504c36fca5045
Thomas <tahjones75@gmail.com>
2020-11-30 08:26:10 +0000

    added AWS S3 cache settings

--- elementsofhealing/settings.py
+++ elementsofhealing/settings.py
@@ -193,6 +193,12 @@ CRIPSY_TEMPLATE_PACK = 'bootstrap4'
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'elementsofhealing'
     AWS_S3_REGION_NAME = 'eu-west-2'
