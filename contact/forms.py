c643039af45d3e6aac5d0bc140134714ae48c86e
Thomas <tahjones75@gmail.com>
2020-11-30 17:16:51 +0000

    no change made

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name= forms.CharField(max_length=50, widget= forms.TextInput(attrs={'class':'form-style-input','placeholder':'Full Name'}))
-    email= forms.CharField(max_length=50, widget= forms.EmailInput(attrs={'class':'form-style-input', 'placeholder':'Email Address'}))
-    message= forms.CharField(max_length=300, widget= forms.Textarea(attrs={'class':'form-style-input','placeholder':'Enter Message'}))
+    name = forms.CharField(max_length=50, widget= forms.TextInput(attrs={'class':'form-style-input','placeholder':'Full Name'}))
+    email = forms.CharField(max_length=50, widget= forms.EmailInput(attrs={'class':'form-style-input', 'placeholder':'Email Address'}))
+    message = forms.CharField(max_length=300, widget= forms.Textarea(attrs={'class':'form-style-input','placeholder':'Enter Message'}))
