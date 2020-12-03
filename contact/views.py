fd951e981a3d8b4350743f9aa1f2f050752816f2
Thomas <tahjones75@gmail.com>
2020-12-03 20:59:13 +0000

    fixing 500 internal error cause by deletion of contact messages

--- contact/views.py
+++ contact/views.py
@@ -13,6 +13,7 @@ from .models import Contact
 def contact(request):
     """ A view to send an email enquires about homeopathic services & information """
     contacts = list(Contact.objects.all().values())
+    print(contacts)
     if request.method == 'GET':
         form = ContactForm()
     else:
@@ -24,6 +25,11 @@ def contact(request):
             date = datetime.today()
             year = datetime.today().year
             for item in contacts:
+                print(int(item['date'].strftime('%Y-%m-%d')[0:4]))
+                print(type(int(item['date'].strftime('%Y-%m-%d')[0:4])))
+                print(year)
+                print(type(year))
+                # print(int(item['date'].strftime('%Y-%m-%d')))
                 if year - int(item['date'].strftime('%Y-%m-%d')[0:4]) > 1:
                     Contact(id=item['id']).delete()
 
