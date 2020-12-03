96e91a71de183235900d3d33343a868883d1ac42
Thomas <tahjones75@gmail.com>
2020-12-03 21:32:21 +0000

    added year field to contact model

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,7 @@ class Contact(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     message = models.TextField(max_length=300, null=True, blank=True)
-    date = models.DateTimeField()
-
+    date = models.DateTimeField(default=datetime.today())
+    year = models.IntegerField(default=datetime.today().year)
     def __str__(self):
         return self.name
