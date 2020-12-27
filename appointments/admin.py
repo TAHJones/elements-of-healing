b69cb70237833569f50640acddbbf52c88e76fd0
Thomas <tahjones75@gmail.com>
2020-12-27 15:27:32 +0000

    updated admin panel settings for appointments

--- appointments/admin.py
+++ appointments/admin.py
@@ -4,12 +4,24 @@ from .models import AppointmentsCalendar
 
 class AppointmentAdmin(admin.ModelAdmin):
     list_display = (
+        'user_profile',
+        'user',
+        'email',
+        'message',
+        'date',
+    )
+
+    fields = (
+        'user_profile',
+        'user',
         'name',
         'email',
         'message',
         'date',
         'date_str',
         'time',
+        'confirmed',
+        'eventId',
     )
 
     ordering = ('date',)
