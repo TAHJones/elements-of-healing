46c3a7d8bacbe0d3591a9c9527550648fc6541e7
Thomas <tahjones75@gmail.com>
2020-12-27 15:29:17 +0000

    addeded more fields including foreign key to AppointmentsCalendar model

--- appointments/models.py
+++ appointments/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from datetime import datetime
 from django.urls import reverse
+from profiles.models import UserProfile
 
 
 class AppointmentsCalendar(models.Model):
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='appointments')
     user = models.CharField(max_length=50, null=False, blank=False)
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
@@ -13,6 +15,8 @@ class AppointmentsCalendar(models.Model):
     time = models.CharField(max_length=10, null=False, blank=False)
     confirmed = models.BooleanField(default=False, null=False, blank=False)
     eventId = models.CharField(max_length=50,  null=True, blank=True)
+    order_number = models.CharField(max_length=32, null=True, blank=True)
+    order_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.user
